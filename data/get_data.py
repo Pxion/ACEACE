4a38df334196cfdac32f2958022353ce1e882083
刘楷 <l2950201560@qq.com>
2025-03-14 18:26:31 +0800

    test(test): 添加测试环境登录功能
    - 新增 app_test.py 文件，用于测试环境下的用户登录- 修改 get_data.py，增加获取最新数据的功能
    - 添加 readme.md 中关于测试环境运行程序的说明
    - 新增 000001.SSTodayData.csv 文件，存储上证指数最新数据
    - 添加 up_down_api.py 文件，用于获取指定日期范围的数据

--- data/get_data.py
+++ data/get_data.py
@@ -1,5 +1,7 @@
 import yfinance as yf
 import pandas as pd
+
+# 下载指定时间段的数据
 def get_data(symbol, start_date, end_date):
     # 下载数据
     dji_data = yf.download(
@@ -18,14 +20,33 @@ def get_data(symbol, start_date, end_date):
     df.drop(1,inplace=True)
 
     # 保存为 CSV 文件
+    df.to_csv(f"{symbol}_{start_date}_{end_date}.csv")
+
+# 获取最新数据
+def get_data(symbol):
+    # 下载数据
+    dji_data = yf.download(
+        symbol,
+        progress=True,     # 显示下载进度条
+        auto_adjust=True   # 使用调整后收盘价（可选）
+    )
+    # 保存为 CSV 文件
+    dji_data.to_csv("testData.csv")
+    df = pd.read_csv("testData.csv")
+    df.drop(0,inplace=True)
+    # dji_data.drop(['Price'], axis=1, inplace=True)
+    df.rename(columns={'Price' : 'Date'}, inplace=True)
+    df.drop(1,inplace=True)
+
+    # 保存为 CSV 文件
     df.to_csv(f"{symbol}_new.csv")
 
 # 设置参数
-symbol = "000001.SS"        # 道琼斯指数代码
+symbol = "^DJI"        # 道琼斯指数代码/上证股指数代码
 start_date = "2012-01-01"
 end_date = "2025-3-11"
 
-get_data(symbol, start_date, end_date)
+get_data(symbol)
 # df=pd.read_csv("testData.csv")
 #
 # # debug
