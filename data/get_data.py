76a535267ab3a6ebc288cac122783245b3ae33d2
30548 <30548515282@qq.com>
2025-03-15 18:15:17 +0800

    修改了会拉取当日数据导致单位不一致的直角交易量问题。

--- data/get_data.py
+++ data/get_data.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import pandas as pd
+from datetime import datetime, timedelta
 
 # 下载指定时间段的数据
 def get_data(symbol, start_date, end_date):
@@ -20,33 +21,35 @@ def get_data(symbol, start_date, end_date):
     df.drop(1,inplace=True)
 
     # 保存为 CSV 文件
-    df.to_csv(f"{symbol}_{start_date}_{end_date}.csv")
+    # df.to_csv(f"{symbol}_{start_date}_{end_date}.csv")
+    df.to_csv(f"{symbol}_new.csv")
 
 # 获取最新数据
-def get_data(symbol):
-    # 下载数据
-    dji_data = yf.download(
-        symbol,
-        progress=True,     # 显示下载进度条
-        auto_adjust=True   # 使用调整后收盘价（可选）
-    )
-    # 保存为 CSV 文件
-    dji_data.to_csv("testData.csv")
-    df = pd.read_csv("testData.csv")
-    df.drop(0,inplace=True)
-    # dji_data.drop(['Price'], axis=1, inplace=True)
-    df.rename(columns={'Price' : 'Date'}, inplace=True)
-    df.drop(1,inplace=True)
-
-    # 保存为 CSV 文件
-    df.to_csv(f"{symbol}_new.csv")
+# def get_data(symbol):
+#     # 下载数据
+#     dji_data = yf.download(
+#         symbol,
+#         progress=True,     # 显示下载进度条
+#         auto_adjust=True   # 使用调整后收盘价（可选）
+#     )
+#     # 保存为 CSV 文件
+#     dji_data.to_csv("testData.csv")
+#     df = pd.read_csv("testData.csv")
+#     df.drop(0,inplace=True)
+#     # dji_data.drop(['Price'], axis=1, inplace=True)
+#     df.rename(columns={'Price' : 'Date'}, inplace=True)
+#     df.drop(1,inplace=True)
+#
+#     # 保存为 CSV 文件
+#     df.to_csv(f"{symbol}_new.csv")
 
 # 设置参数
 symbol = "000001.SS"        # 道琼斯指数代码/上证股指数代码000001.SS
 start_date = "2012-01-01"
-end_date = "2025-3-11"
+# end_date = "2025-3-15"
+end_date = datetime.today().date() - timedelta(days=1)
 
-get_data(symbol)
+get_data(symbol, start_date, end_date)
 # df=pd.read_csv("testData.csv")
 #
 # # debug
