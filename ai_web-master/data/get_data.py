699aea22814a974f027d4f38fc3025e0d6d98c7b
Cui Wen Huo <154602185+CYM134@users.noreply.github.com>
2025-03-15 14:52:44 +0800

    Add files via upload

---
+++ ai_web-master/data/get_data.py
@@ -0,0 +1,54 @@
+import yfinance as yf
+import pandas as pd
+
+# 下载指定时间段的数据
+def get_data(symbol, start_date, end_date):
+    # 下载数据
+    dji_data = yf.download(
+        symbol,
+        start=start_date,
+        end=end_date,
+        progress=True,     # 显示下载进度条
+        auto_adjust=True   # 使用调整后收盘价（可选）
+    )
+    # 保存为 CSV 文件
+    dji_data.to_csv("testData.csv")
+    df = pd.read_csv("testData.csv")
+    df.drop(0,inplace=True)
+    # dji_data.drop(['Price'], axis=1, inplace=True)
+    df.rename(columns={'Price' : 'Date'}, inplace=True)
+    df.drop(1,inplace=True)
+
+    # 保存为 CSV 文件
+    df.to_csv(f"{symbol}_{start_date}_{end_date}.csv")
+
+# 获取最新数据
+def get_data(symbol):
+    # 下载数据
+    dji_data = yf.download(
+        symbol,
+        progress=True,     # 显示下载进度条
+        auto_adjust=True   # 使用调整后收盘价（可选）
+    )
+    # 保存为 CSV 文件
+    dji_data.to_csv("testData.csv")
+    df = pd.read_csv("testData.csv")
+    df.drop(0,inplace=True)
+    # dji_data.drop(['Price'], axis=1, inplace=True)
+    df.rename(columns={'Price' : 'Date'}, inplace=True)
+    df.drop(1,inplace=True)
+
+    # 保存为 CSV 文件
+    df.to_csv(f"{symbol}_new.csv")
+
+# 设置参数
+# symbol = "^DJI"        # 道琼斯指数代码/上证股指数代码
+symbol = "000001.SS"
+start_date = "2012-01-01"
+end_date = "2025-3-13"
+
+get_data(symbol)
+# df=pd.read_csv("testData.csv")
+#
+# # debug
+# df.drop(0, inplace=True)
